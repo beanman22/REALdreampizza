d7ecb083dc40731359f0e931ba1d88b1c426b9aa
Louis roulston <louisroulston44@gmail.com>
2023-05-16 21:04:08 +1200

    Adding basic variables, constants, and arrays

--- actualdreampizza/Form1.vb
+++ actualdreampizza/Form1.vb
@@ -2,3 +2,15 @@
 Imports System.Windows
 Imports System.Windows.Forms.DataFormats
 
+Public Class Form1
+
+    Const STANDARDPRICE As Decimal = 7.5 'declares the price of a standard pizza
+    Const GOURMETPRICE As Decimal = 14.5 'declares the price of a gourmet pizza
+
+    Public total As Decimal = 0 'declares variable for the total cost of all pizzas
+    Public delivery As Boolean 'declares variable for the delivery option
+
+    Public details(7) As String 'declares a one dimensional array for the customer details
+    Public pizza(9, 3) As String 'declares 2d array for pizza details
+    Public totals(10) As Decimal 'declares 1d array for final addition of pizza cost and delivery cost
+End Class
