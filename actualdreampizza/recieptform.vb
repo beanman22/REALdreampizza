93f50b11fca847f487faaa3b5c5ebd497ee05ef0
Louis Roulston <21181@jpc.school.nz>
2023-05-31 13:21:16 +1200

    recipt form almost done

--- actualdreampizza/recieptform.vb
+++ actualdreampizza/recieptform.vb
@@ -6,28 +6,31 @@ Public Class recieptform
     End Sub
     Private Sub recieptform_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
         Dim storedetails As String
         storedetails = My.Computer.FileSystem.ReadAllText("storedetails.txt")
-        LblReceipt.Text += storedetails & vbCrLf & "***" & vbCrLf & vbCrLf
-
-        LblReceipt.Text += DateString & "  " & TimeOfDay & vbCrLf & "*******************************" & vbCrLf & vbCrLf
+        LblReceipt.Text += storedetails & vbCrLf & vbCrLf & "*********************************" & vbCrLf
+        LblReceipt.Text += DateString & "  " & TimeOfDay & vbCrLf & "*********************************" & vbCrLf & vbCrLf
 
 
         LblReceipt.Text += "CUSTOMER DETAILS:" & vbCrLf
         LblReceipt.Text += Form1.details(0) & " " & Form1.details(1) & vbCrLf & Form1.details(2)
         LblReceipt.Text += vbCrLf & vbCrLf & "DELIVERY DETAILS:" & vbCrLf
 
+
         For i = 3 To 6
             LblReceipt.Text += Form1.details(i) & vbCrLf
         Next
 
-        LblReceipt.Text += vbCrLf & "*******************************" & vbCrLf & vbCrLf
+        LblReceipt.Text += vbCrLf & "*********************************" & vbCrLf & vbCrLf
 
         For i = 0 To 11
             If Val(Form1.pizza(i, 2)) > 0 Then
-                LblReceipt.Text += Form1.pizza(i, 2) & "x " & Form1.pizza(i, 0) & "        " & FormatCurrency(Form1.pizza(i, 3)) & vbCrLf
+                LblReceipt.Text += Form1.pizza(i, 2) & "x   " & Form1.pizza(i, 0) & "     " & FormatCurrency(Form1.pizza(i, 3)) & vbCrLf
             End If
         Next
+
+        LblReceipt.Text += vbCrLf & "*********************************" & vbCrLf & vbCrLf & "total cost: " & FormatCurrency(Form1.total)
 
     End Sub
 End Class
